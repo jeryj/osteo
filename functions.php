8e0ad7ebd086a27343430a6acde8d2cf0d748bdc
Jerry <jones.jeremydavid@gmail.com>
2014-01-24 13:56:00 -0600

    condensed scripts

--- functions.php
+++ functions.php
@@ -114,10 +114,8 @@ add_action( 'widgets_init', 'messenger_pigeons_widgets_init' );
 function messenger_pigeons_scripts() {
 	wp_enqueue_style( 'messenger_pigeons-font-awesome', get_template_directory_uri() . '/icons/css/font-awesome.css' );
 	wp_enqueue_style( 'messenger_pigeons-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'messenger_pigeons-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
 	wp_enqueue_script( 'messenger_pigeons-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+    wp_enqueue_script( 'messenger_pigeons-scripts', get_template_directory_uri() . '/js/scripts.js', array(), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * scripts.js
+ *
+ * General site scripts
+ */
+
+jQuery( document ).ready( function( $ ) {
+
+    //Navigation
+    $('.menu-toggle').click(function() {
+        $('.main-navigation').toggleClass('toggled');
+    });
+
+
+
+});
