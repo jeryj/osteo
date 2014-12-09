35e7e1b8b02e4077bf0e1e3b42b29cddd282bf97
Jerry <jones.jeremydavid@gmail.com>
2014-12-09 10:03:40 -0600

    moved sidebar down a bit and removed showing search field if sidebar is empty

--- sidebar.php
+++ sidebar.php
@@ -6,11 +6,7 @@
  */
 ?>
 	<div id="secondary" class="widget-area" role="complementary">
-		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-			<aside id="search" class="widget widget_search">
-				<?php get_search_form(); ?>
-			</aside>
+			<?php dynamic_sidebar( 'sidebar-1' );?>
 
-		<?php endif; // end sidebar widget area ?>
 	</div><!-- #secondary -->
