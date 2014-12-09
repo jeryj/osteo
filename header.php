02831e335d6d95b703df9d0f3c8b3b96ea66ce09
Jerry <jones.jeremydavid@gmail.com>
2014-12-09 10:56:51 -0600

    removed site description in header because we've never used it

--- header.php
+++ header.php
@@ -22,10 +22,7 @@
 <?php do_action( 'before' ); ?>
 <header id="masthead" class="site-header full-width padding-top" role="banner">
 	<div class="container">
-		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h3 class="menu-toggle"><i class="fa-bars btn"></i></h3>
