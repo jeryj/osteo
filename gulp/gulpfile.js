d492bda61e9e1a61008603e6de351daa07022375
Jerry <jones.jeremydavid@gmail.com>
2015-03-19 15:19:51 -0500

    moved gulp processes into one folder

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('sass', function () {
   // gulp.src locates the source files for the process.
   // This globbing function tells gulp to use all files
   // ending with .scss or .sass within the scss folder.
-  gulp.src('stylesheets/*.{scss,sass}')
+  gulp.src('../stylesheets/*.{scss,sass}')
     // Initializes sourcemaps
     .pipe(sourcemaps.init())
     // Converts Sass into CSS with Gulp Sass
@@ -16,14 +16,14 @@ gulp.task('sass', function () {
     // Writes sourcemaps into the CSS file
     .pipe(sourcemaps.write())
     // Outputs CSS files in the css folder
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('../'));
 })
 
 // Watch scss folder for changes
 gulp.task('watch', function() {
   // Watches the scss folder for all .scss and .sass files
   // If any file changes, run the sass task
-  gulp.watch('stylesheets/*.{scss,sass}', ['sass'])
+  gulp.watch('../stylesheets/*.{scss,sass}', ['sass'])
 })
 
 // Creating a default task
